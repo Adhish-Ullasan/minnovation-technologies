40d6a105b6fbfb002d709b04351e55fa7b4d71a0
Adhish <adhishullasan@gmail.com>
2025-07-31 12:25:04 +0530

    HOMEPAGE : complete connect and collaboration contents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,27 +20,30 @@ const Home = () => {
                   <button className='primaryButton'>LEARN MORE</button>
                 </div>
             </div>
-            <div className="connect">
-                <div>
-                  <h2>Connect and collaborate from asingle pane of glass.</h2>
-                </div>
-                <div>
-                    <div>
-                      <img src="/images/home/Analytics.svg" alt="analytics" />
-                    </div>
-                    <div>
-                      <p>With AlphaX™ you can seamlessly connect your data from LPWAN, fieldbus and <br /> 
-                        other time-series sources with the decision makers in your teams. You can easily <br />
-                        create and share mind-blowing visualisations and put the right information into <br />
-                        the hands of the right people at the right time. AlphaX will help you accelerate the <br />
-                        work of your data scientists and business analysts… and it will do all of this from a <br />
-                        single pane of glass.</p>
-                    </div>
-                    <div>
-                      <button className='primaryButton'>GET STARTED</button>
+            <div className="wrapper">
+              <div></div>
+              <div className="connect">
+                  <div>
+                    <h2>Connect and collaborate from asingle pane of glass.</h2>
+                  </div>
+                  <div>
+                      <div>
+                        <img src="/images/home/Analytics.svg" alt="analytics" />
+                      </div>
+                      <div>
+                        <p>With AlphaX™ you can seamlessly connect your data from LPWAN, fieldbus and <br /> 
+                          other time-series sources with the decision makers in your teams. You can easily <br />
+                          create and share mind-blowing visualisations and put the right information into <br />
+                          the hands of the right people at the right time. AlphaX will help you accelerate the <br />
+                          work of your data scientists and business analysts… and it will do all of this from a <br />
+                          single pane of glass.</p>
+                          <div>
+                            <button className='primaryButton'>GET STARTED</button>
+                          </div>
+                      </div>
                     </div>
                   </div>
-            </div>
+              </div>
           </div>
         </>
     )
