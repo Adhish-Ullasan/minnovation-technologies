972bf737194681bcf80f53b1b03a8f55e9c666b7
Adhish <adhishullasan@gmail.com>
2025-07-31 11:39:20 +0530

    HOMEPAGE : add initial contents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,21 @@ const Home = () => {
 
     return (
         <>
-          <div cl></div>
+          <div className="container">
+            <div>
+              <p>AlphaX Data Platform™</p>
+            </div>
+            <div>
+              <h2>Edge to Insight</h2>
+            </div>
+            <div>
+              <p>vTransform data from sensors and field devices <br />
+                into insights for your team.</p>
+            </div>
+            <div>
+              <button className='primaryButton'>LEARN MORE</button>
+            </div>
+          </div>
         </>
     )
   }
