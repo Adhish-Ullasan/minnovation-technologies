2b8b5aaa76ac8c70565a2cbb106947481ddf3dd6
Adhish <adhishullasan@gmail.com>
2025-08-01 11:47:38 +0530

    service : create servicecard data array

--- src/components/ServiceCards.jsx
+++ src/components/ServiceCards.jsx
@@ -1,8 +1,16 @@
-const serviceCards = () => {
+const ServiceCards = () => {
     return (
-        <>
-        </>
+         <div className="serviceCards">
+                        <div>
+                            <div className="serviceImage">
+                                <img src="/images/home/datamanagement.svg" alt="data management" />
+                            </div>
+                            <div>
+                                <h5>Data Management System</h5>
+                            </div>
+                        </div>
+                </div>
     )
 }
 
-export default serviceCards
+export default ServiceCards
---
+++ src/components/ServiceData.jsx
@@ -0,0 +1,28 @@
+const ServiceData = [
+    {
+        Image:"/public/images/home/datamanagement.svg",
+        serviceName: "Data Management"
+    },
+    {
+        Image:"/public/images/home/cloudconnectivity.svg",
+        serviceName: "Edge To Cloud Connectivity"
+    },
+    {
+        Image:"/public/images/home/videoanalysis.svg",
+        serviceName: "Real Time Video Analytics"
+    },
+    {
+        Image:"/public/images/home/happysmillingwoman.svg",
+        serviceName: "Sensing As a Service Managed Service"
+    },
+    {
+        Image:"/public/images/home/vendor.svg",
+        serviceName: "lot Vendor Market Place"
+    },
+    {
+        Image:"/public/images/home/pleasuedoingbusinnes.svg",
+        serviceName: "Partners,Resellers and Integrators"
+    },
+ ]
+
+ export default ServiceData
